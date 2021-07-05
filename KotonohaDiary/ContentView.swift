bd63acdfc97d3e80a61ae5c917970667afc565d6
techiro <techiromen0@gmail.com>
2021-07-05 12:11:34 +0900

    Implementation of TabView.

--- KotonohaDiary/ContentView.swift
+++ KotonohaDiary/ContentView.swift
@@ -8,12 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var selectedView = 1
+    
     var body: some View {
-        Text("Hello, world!")
+        TabView(selection: $selectedView) {
+            
+            Button("Show Second View") {
+                selectedView = 2
+            }
             .padding()
+            .tabItem {
+                Label("First", systemImage: "1.circle")
+            }
+            .tag(1)
+            
+            Button("Show First View") {
+                selectedView = 1
+            }
+            .padding()
+            .tabItem {
+                Label("Second", systemImage: "2.circle")
+            }
+            .tag(2)
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
