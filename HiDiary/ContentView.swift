9be91ae445c0df26c10921e5cd7c99feda63e877
techiro <techiromen0@gmail.com>
2021-07-08 11:42:43 +0900

    add: JournalListView and JournalRowView. modify: disabled force_try from SwiftLint rule.

--- HiDiary/ContentView.swift
+++ HiDiary/ContentView.swift
@@ -11,27 +11,30 @@ struct ContentView: View {
     @State var selectedView = 1
     @State private var tabViewOffset: CGFloat = 0
     var body: some View {
-        TabView(selection: $selectedView) {
-            
-            Button("Show Second View") {
-                selectedView = 2
-            }
-            .padding()
-            .tabItem {
-                Label("First", systemImage: "1.circle")
-            }
-            .tag(1)
-            
-            Button("Show First View") {
-                selectedView = 1
-            }
-            .padding()
-            .tabItem {
-                Label("Second", systemImage: "2.circle")
-            }
-            .tag(2)
-        }
-        .overlay(FloatingActionButton(bottomPadding: 60), alignment: .bottom)
+        
+        JournalListView()
+                
+//        TabView(selection: $selectedView) {
+//
+//            Button("Show Second View") {
+//                selectedView = 2
+//            }
+//            .padding()
+//            .tabItem {
+//                Label("First", systemImage: "1.circle")
+//            }
+//            .tag(1)
+//
+//            Button("Show First View") {
+//                selectedView = 1
+//            }
+//            .padding()
+//            .tabItem {
+//                Label("Second", systemImage: "2.circle")
+//            }
+//            .tag(2)
+//        }
+//        .overlay(FloatingActionButton(bottomPadding: 60), alignment: .bottom)
     }
     
 }
