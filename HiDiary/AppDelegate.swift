be06a1f842a71d84550f48863b29bb985a061d5d
techiro <techiromen0@gmail.com>
2021-07-19 09:28:04 +0900

    add: FirebaseApp configure.

--- HiDiary/AppDelegate.swift
+++ HiDiary/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by TanakaHirokazu on 2021/07/03.
 //
 
+import Firebase
 import UIKit
 
 @main
@@ -12,7 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        FirebaseApp.configure()
         return true
     }
     
