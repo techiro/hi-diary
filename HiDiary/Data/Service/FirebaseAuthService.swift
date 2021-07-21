dde07c49b0d9f0c095ba5962cc17dcee9853fa2d
techiro <techiromen0@gmail.com>
2021-07-21 15:30:10 +0900

    add: FirebaseAuthenticationService.

---
+++ HiDiary/Data/Service/FirebaseAuthService.swift
@@ -0,0 +1,53 @@
+//
+//  FirebaseAuthService.swift
+//  HiDiary
+//
+//  Created by TanakaHirokazu on 2021/07/20.
+//
+
+import FirebaseAuth
+import Foundation
+
+class FirebaseAuthenticationService: ObservableObject {
+    @Published var user: User?
+    
+    var cancellable: AuthStateDidChangeListenerHandle?
+    
+    init() {
+        cancellable = Auth.auth().addStateDidChangeListener { (_, user) in
+            if let user = user {
+                self.user = User(
+                    uid: user.uid,
+                    email: user.email,
+                    displayName: user.displayName
+                )
+            } else {
+                
+                self.user = nil
+            }
+        }
+    }
+    
+    func signUp(
+        email: String,
+        password: String,
+        handler: @escaping AuthDataResultCallback
+    ) {
+        Auth.auth().createUser(withEmail: email, password: password, completion: handler)
+    }
+    
+    func signIn(
+        email: String,
+        password: String,
+        handler: @escaping AuthDataResultCallback
+    ) {
+        Auth.auth().signIn(withEmail: email, password: password, completion: handler)
+    }
+    
+    func signOut() throws {
+        try Auth.auth().signOut()
+        self.user = nil
+        throw FirebaseAuthError.signOutError
+    }
+    
+}
