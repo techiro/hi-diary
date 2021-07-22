a18496cffdf314535a18f48f679a156fde3ae7e6
kubo <kubo.shogo@arakawa-lab.com>
2021-07-22 18:16:04 +0900

    add ChatView

---
+++ HiDiary/Chat/ChatView.swift
@@ -0,0 +1,20 @@
+//
+//  ChatView.swift
+//  HiDiary
+//
+//  Created by arakawaLab on 2021/07/22.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChatView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatView()
+    }
+}
