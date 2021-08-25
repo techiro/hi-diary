cdb04fd48effb140fa21d594c265e16fc80785d4
kubo <kubo.shogo@arakawa-lab.com>
2021-08-25 11:43:30 +0900

    添削Viewフォルダとファイル作成

---
+++ HiDiary/UI/View/Correction/CorrectionView.swift
@@ -0,0 +1,20 @@
+//
+//  CorrectionView.swift
+//  HiDiary
+//
+//  Created by arakawaLab on 2021/08/25.
+//
+
+import SwiftUI
+
+struct CorrectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CorrectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        CorrectionView()
+    }
+}
